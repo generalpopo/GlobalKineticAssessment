99b8d0c1e0e889bb3fcd404931a8cf4a851be744
Comfort Mokoena <mokoenacd@gmail.com>
2020-12-04 10:30:18 +0200

    Adding model

---
+++ GlobalKineticAssessment/Models/ICoin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalKineticAssessment.Models
+{
+    public interface ICoin
+    {
+        decimal Amount { get; set; }
+        decimal Volume { get; set; }
+    }
+}
---
+++ GlobalKineticAssessment/Models/UsCoin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalKineticAssessment.Models
+{
+    public class UsCoin : ICoin
+    {
+        const int STANDARD_VOLUME = 42;
+        public UsCoin(decimal Amount)
+        {
+            Volume = Amount * STANDARD_VOLUME;
+            this.Amount = Amount;
+        }
+        public decimal Amount { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
